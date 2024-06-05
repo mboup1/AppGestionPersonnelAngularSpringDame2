e32e14f78c433786f64d978f362386b42eb03d05
mboup <mboup124@gmail.com>
2024-06-05 19:54:51 +0200

    modif repo

--- appGestionEmployesSpringBoot/src/main/java/com/AppGestionEmployes/appEmployes/repository/EmployeeRepository.java
+++ appGestionEmployesSpringBoot/src/main/java/com/AppGestionEmployes/appEmployes/repository/EmployeeRepository.java
@@ -9,6 +9,7 @@ public interface EmployeeRepository extends MongoRepository<Employee, String> {
 }
 
 
+
 //@Repository
 //public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 //}
