f2c9d2964e2eee3a5eb412c5226ed122c8febafa
mboup <mboup124@gmail.com>
2024-06-05 21:49:12 +0200

    updated

--- appGestionEmployesSpringBoot/src/main/java/com/AppGestionEmployes/appEmployes/repository/EmployeeRepository.java
+++ appGestionEmployesSpringBoot/src/main/java/com/AppGestionEmployes/appEmployes/repository/EmployeeRepository.java
@@ -7,9 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 }
-
-
-
-//@Repository
-//public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-//}
